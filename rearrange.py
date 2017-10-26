ddd1d559e386b0f5e8ea0b15388f0a0e40e0cae2
Sunny Ouyang <shenny@Sunnys-MacBook-Pro.local>
2017-10-26 14:23:33 -0700

    fixed rearrange

--- rearrange.py
+++ rearrange.py
@@ -5,9 +5,16 @@ words = ["Kaichi", "Chris", "Poops", "lots", "of", "eats", "super"]
 
 def create_quote():
     word = []
-    for x in range(len(words)):
+    used_random_numbers = []
+    number_of_words = 0
+    while number_of_words < len(words):
         rand_index = random.randint(0, len(words) - 1)
-        word.append(words[rand_index])
+
+        if rand_index not in used_random_numbers:
+            word.append(words[rand_index])
+            used_random_numbers.append(rand_index)
+            number_of_words +=1
+
 
     return word
 
