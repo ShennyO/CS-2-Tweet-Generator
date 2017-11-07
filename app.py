ad14dbdec8f70d6e854c48451d380f7703dfdeea
Sunny Ouyang <shenOuyang.work@gmail.com>
2017-11-07 12:27:28 -0800

    testing

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
-import dictionary_words
+# import dictionary_words
 
 app = Flask(__name__)
 
 @app.route('/')
 def return_random_sentence():
-    sentence = dictionary_words.generate_random_sentence(20)
-    return sentence
+    # sentence = dictionary_words.generate_random_sentence(20)
+    return "hello"
+
+
+
+
+if __name__ == “__main__“:
+   app.run(debug=True)
